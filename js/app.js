582c38419b78b04503f4c00f34170709e8b68165
Garam Jang <garamj88@gmail.com>
2022-02-04 01:54:17 -0500

    solve levelup 1

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 
 // 5. Create cached ele ref for each ele with its id - button, input, ul
-  // (moved input, ul inside the sbmtBtn)
+// (moved input, ul inside the sbmtBtn)
 const sbmtBtn = document.getElementById('submit-button');
 
 // 6. addEventListner to the sbmtBtn to click evt, when clicked
-sbmtBtn.addEventListener('click', function(evt) {
+sbmtBtn.addEventListener('click', function (evt) {
   const toDoIpt = document.getElementById('todo-ipt');
 
   // create a new element: li (= newLi)
-  const newLi = document.createElement('li');  
+  const newLi = document.createElement('li');
   // set new li to the input value
   newLi.textContent = toDoIpt.value;
-  
+
+  // Level-up:  do not add empty entry to the toDoLi => only add string that is not empty
+  if (toDoIpt.value !== "") {
+    const toDoLi = document.getElementById('todo-list');
+    // add the new li to/inside ul (=toDoLi)
+    toDoLi.appendChild(newLi);
+  }
   // reset input to an empty str
   toDoIpt.value = "";
-
-  const toDoLi = document.getElementById('todo-list');
-  // add the new li to/inside ul (=toDoLi)
-  toDoLi.appendChild(newLi);
 });
